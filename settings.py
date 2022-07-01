82fc39c5a05e5826bab647d5247283d41f7f3548
Shefishefo <86641801+Shefishefo@users.noreply.github.com>
2022-07-01 21:43:07 +0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+token = "5362155984:AAF74hdjMFPWDkzkCmv-Cg-u2mrF-DGKbUk"
+botname = 'crasivorobot'
+channel_id = -1001612614271 # айди канала
+link_channel = "babochkiii" # сылка на канал
+
+QIWI_ACCOUNT = '79633167050' # Номер кошелька без +
+p2p_qiwi_public_key = "4dbb4baf8fa1322ed9d956097e8e56e9"
+p2p_qiwi_secret_key = "="
+admins = [5065943400, 2075004315, 1886432808] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 1 #минимум сабкоинов для раскрутки канала
+ref_count = 3 #реферальный бонус (сабкоины)
+REF_BONUS = 3
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-01-16' #ссылка на правила и инструкцию
